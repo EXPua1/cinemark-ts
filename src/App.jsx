5b2a5e93ae37311b5b3200b156fac29079c06a1f
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2025-02-17 15:44:04 +0200

    created redux and routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 
+import { Link, Route, Routes } from 'react-router-dom'
 import './App.css'
+import MoviesPage from './pages/MoviesPage/MoviesPage'
+import HomePage from './pages/HomePage/Home'
 
 function App() {
- 
+
 
   return (
-   <div>hello</div>
+    <>
+      <div>try this routing</div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/movies">Movies</Link></li>
+      </ul>
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+      </Routes>
+    </>
+
   )
 }
 
---
+++ src/redux/auth/slice.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  token: null,
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.user = action.payload;
+    },
+    setToken: (state, action) => {
+      state.token = action.payload;
+    },
+  },
+});
+
+export const { setUser, setToken } = authSlice.actions;
+export const auth = authSlice.reducer;
