1926a9801664e19ad23be46a8c8aa50b432d5357
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2025-02-18 15:40:57 +0200

    add

--- src/components/Hero/HeroList/HeroList.jsx
+++ src/components/Hero/HeroList/HeroList.jsx
@@ -1,27 +1,59 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import css from "./HeroList.module.css";
+import { genres } from "../../constants/genres";
 
-const defImg = "https://via.placeholder.com/200"; 
+const defImg = "https://via.placeholder.com/200";
+
+
+
 
 const HeroList = ({ films }) => {
-   
+    console.log(films)
+    const swiperRef = useRef(null);
+
+    const getGenres = (genreIds) => {
+        return genreIds
+            .map((id) => genres.find((genre) => genre.id === id))  // Находим жанры по ID
+            .map((genre) => genre?.name)  // Извлекаем имя жанра
+            .join(', ');  // Объединяем жанры в строку
+    };
+
+
     if (!Array.isArray(films) || films.length === 0) {
-        return <p>No films available.</p>; 
+        return <p>No films available.</p>;
     }
 
     return (
         <div className={css.container}>
-            <h2 className={css.title}>Films list</h2>
+
+            <div className={css.btn_swipe}>
+                <h2 className={css.title}>Films list</h2>
+                <div className={css.customNavigation}>
+                    <button
+                        className={css.prevButton}
+                        onClick={() => swiperRef.current.swiper.slidePrev()}
+                    >
+                        &#8592; {/* Стрелка влево */}
+                    </button>
+                    <button
+                        className={css.nextButton}
+                        onClick={() => swiperRef.current.swiper.slideNext()}
+                    >
+                        &#8594; {/* Стрелка вправо */}
+                    </button>
+                </div>
+            </div>
+
             <Swiper
+                ref={swiperRef}
                 modules={[Navigation, Pagination]}
-                spaceBetween={0} 
+                spaceBetween={10}
                 slidesPerView={4}
-                navigation
                 pagination={{ clickable: true }}
                 loop={true}
             >
@@ -33,7 +65,14 @@ const HeroList = ({ films }) => {
                                 alt={film.title}
                                 width={200}
                             />
-                            <p>{film.title}</p>
+                            <div className={css.info}>
+                                <p>{film.title}</p>
+
+                                <span className={css.rating}>{film.vote_average}</span>
+                                <span>{film.rating}</span>
+                                <span>{getGenres(film.genre_ids)}</span>
+                            </div>
+
                         </div>
                     </SwiperSlide>
                 ))}
