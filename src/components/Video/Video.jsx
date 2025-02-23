f6678bb58cf57ea420b74981006c339829f88d70
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2025-02-23 14:32:41 +0200

    player

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import css from './Video.module.css';
 
 import {
   fetchMovieTrailer,
@@ -25,13 +26,16 @@ const Video = ({ id, type }) => {
       <iframe
         width="560"
         height="315"
-        src={`https://www.youtube.com/embed/${
-          type === 'movie' ? videoKey : tvVideoKey
-        }`}
+        src={`https://www.youtube.com/embed/${type === 'movie' ? videoKey : tvVideoKey}?autohide=1&controls=0&modestbranding=1&rel=0&showinfo=0&fs=0&iv_load_policy=3&enablejsapi=1`}
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        style={{
+          borderRadius: '12px', 
+          boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)',
+          overflow: 'hidden', 
+        }}
       ></iframe>
     </div>
   );
