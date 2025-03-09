8da21bed8858ab23c293898eded24ff8fc1e63bc
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2025-03-09 14:38:10 +0200

    add rating list visual styles

--- src/components/Rating/RatingItem/RatingItem.jsx
+++ src/components/Rating/RatingItem/RatingItem.jsx
@@ -1,23 +1,46 @@
-import { Link } from 'react-router';
-
+import { Link, useLocation } from 'react-router';
+import { GoInfo } from "react-icons/go";
 import css from './RatingItem.module.css';
 
 const RatingItem = ({ title, films }) => {
+  const location = useLocation();
   return (
     <div>
       <h2>{title}</h2>
       <div className={css.container}>
-        <ul>
-          {films.map(film => {
+        <ul className={css.list}>
+          {films.map((film, index) => {
             const type = film.media_type || 'movie';
 
             return (
               <li className={css.item} key={film.id}>
-                <Link to={`/${type}/${film.id}`}>
-                  <p>{film.title || film.name}</p>
+                <div className={css.itemCont}>
+                  <div className={css.imgCont}>
+                    <img className={css.img} src={
+                      film.poster_path
+                        ? `https://image.tmdb.org/t/p/w200${film.poster_path}`
+                        : defImg
+                    }></img>
+                  </div>
+                  <div>
+
+                    <p>  {index + 1}. {film.title || film.name}</p>
+                    <div className={css.year}>
+                      <p>{film.release_date?.split('-')[0] || film.first_air_date?.split('-')[0]}</p>
+                      <p>{film.runtime}</p>
+                      {console.log(film)}
+                    </div>
+
+                    <p className={css.rating}>{film.vote_average.toFixed(1)} ({film.vote_count })</p>
+                  </div>
+
+                </div>
+                <Link from={location} to={`/${type}/${film.id}`}>
+
+                  <GoInfo />
                 </Link>
 
-                <p className={css.rating}>{film.vote_average.toFixed(1)}</p>
+
               </li>
             );
           })}
