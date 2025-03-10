d75979b254561fd24dfac16a0488478a791a9b2c
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2025-03-10 23:33:35 +0200

    changed React Player add there light for poster, deleted useless div

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -7,54 +7,28 @@ import css from './Video.module.css';
 import { selectFilteredTrailers } from '../../redux/movies/selectors';
 
 const Video = ({ id, type, posterPath }) => {
-  const [isPlayerActive, setIsPlayerActive] = useState(false); 
+  const [isPlayerActive, setIsPlayerActive] = useState(false);
   const { tvVideoKey, videoKey, loading } = useSelector(selectFilteredTrailers);
-  console.log(posterPath)
   const videoUrl = type === 'movie' ? videoKey : tvVideoKey;
 
-  
   const handlePlayClick = () => {
     if (videoUrl) {
-
       setIsPlayerActive(true);
     }
   };
 
   return (
     <div className={`${css.container} ${isPlayerActive ? css.fadeIn : ''}`}>
-      {isPlayerActive && videoUrl ? (
-        <ReactPlayer
-          url={`https://www.youtube.com/watch?v=${videoUrl}`}
-          controls
-          playing={true} 
-          width="100%"
-          height="100%"
-        />
-      ) : (
-        <div
-          style={{
-            backgroundImage: posterPath ? `url(${IMAGE_URL}${posterPath})` : 'none',
-            backgroundSize: 'cover', 
-            backgroundPosition: 'center', 
-          }}
-            className={`${css.placeholder} ${isPlayerActive ? css.fadeOut : ''}`}
-          onClick={handlePlayClick}
-          role="button"
-          tabIndex={0} 
-          onKeyDown={(e) => e.key === 'Enter' && handlePlayClick()}
-        >
-          {loading ? (
-            <p>Loading...</p>
-          ) : videoUrl ? (
-            <>
-              <div className={css.playIcon}>▶</div>
-              <p>Watch Trailer</p>
-            </>
-          ) : (
-            <p>Not avaible</p>
-          )}
-        </div>
-      )}
+      <ReactPlayer
+        url={videoUrl ? `https://www.youtube.com/watch?v=${videoUrl}` : null}
+        controls
+        playing={isPlayerActive}
+        width="100%"
+        height="100%"
+        light={posterPath ? `${IMAGE_URL}${posterPath}` : true}
+        onClickPreview={handlePlayClick}
+        style={{borderRadius: '8px', overflow: 'hidden'}}
+      />
     </div>
   );
 };
